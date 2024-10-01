74af5e887f269f1b31759c6609f40c5054f66cee
SampoL <sampojolehtonen@gmail.com>
2024-10-01 16:16:59 +0300

    Price estimation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import imageRouter from "./routes/image";
+import priceRouter from "./routes/price";
 
 const app = express();
 const PORT = 3000;
@@ -10,6 +11,7 @@ app.get("/ping", (_req: Request, res: Response) => {
 });
 
 app.use("/api/image", imageRouter);
+app.use("/api/price", priceRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ src/services/priceService.ts
@@ -0,0 +1,53 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import dotenv from "dotenv";
+import { PriceAnalysisResponse, FurnitureDetails} from "../types";
+
+dotenv.config();
+
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+
+if (!GEMINI_API_KEY) {
+  throw new Error("GEMINI_API_KEY is not set in the environment variables");
+}
+
+const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const createPrompt = (furnitureDetails: FurnitureDetails) => `
+  Description of the piece of furniture in the photo:
+
+  The type of the furniture is ${furnitureDetails.type}. The maker of the furniture is ${furnitureDetails.brand} and its model is ${furnitureDetails.model}.
+  The color of the furniture is ${furnitureDetails.color}. The approximate dimensions are ${furnitureDetails.dimensions.length}x${furnitureDetails.dimensions.width}x${furnitureDetails.dimensions.height} cm.
+  The condition is ${furnitureDetails.condition}. The age is ${furnitureDetails.age} years.
+
+  Provide a price estimate for my piece of furniture in the second-hand market, based on this description and the photo. The second-hand market is based in Finland.
+  Return 3 prices and 1 description as a JSON object with the fields: highest_price, lowest_price, average_price, description, and sell_probability.
+  The sell_probability should estimate how probable the item will sell and which price is most likely to succeed. Format the response as a consistent JSON object.
+`;
+
+const parsePriceResponse = (responseText: string) => {
+  const cleanedText = responseText.replace(/```json\n?|\n?```/g, "").trim();
+  return JSON.parse(cleanedText) as PriceAnalysisResponse;
+};
+
+const analyzePriceEstimate = async (imageBase64: string, furnitureDetails: FurnitureDetails) => {
+  const prompt = createPrompt(furnitureDetails);
+
+  try {
+    const result = await model.generateContent([
+      { text: prompt },
+      { inlineData: { mimeType: "image/jpeg", data: imageBase64 } },
+    ]);
+    const response = result.response;
+    const text = response.text();
+
+    const parsedResponse = parsePriceResponse(text);
+    
+    return parsedResponse;
+
+  } catch (error) {
+    return { error: "An unexpected error occurred during price analysis." };
+  }
+};
+
+export default analyzePriceEstimate;
