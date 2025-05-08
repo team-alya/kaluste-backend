a672ba0455a954476c18f83de826266974899d65
laguagu <AkuLeskinen@hotmail.com>
2025-05-08 12:59:09 +0300

    Update(middleware) time out timer

--- src/middleware/timeout.ts
+++ src/middleware/timeout.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
-
 export const timeout = (req: Request, res: Response, next: NextFunction) => {
-  req.setTimeout(180000); // 3min
-  res.setTimeout(180000); // 3min
+  const sevenMinutesInMs = 7 * 60 * 1000; // 7min (420000ms)
+  req.setTimeout(sevenMinutesInMs);
+  res.setTimeout(sevenMinutesInMs);
   next();
 };
